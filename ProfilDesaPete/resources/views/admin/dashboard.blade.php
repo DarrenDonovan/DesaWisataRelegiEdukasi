d2a9f1187b3cf37f5c7f850aa3104e06f629343c
Darren Donovan <127038694+DarrenDonovan@users.noreply.github.com>
2025-04-07 15:00:06 +0700

    Sidebar and Topbar Adjustments
    - Memperbaiki positioning dari sidebar dan topbar agar tumpang tindih dengan elemen lain
    - Memperbaiki linkage anchor dari sidebar yang sebelumnya tidak dapat diklik karena tumpang tindih

--- ProfilDesaPete/resources/views/admin/dashboard.blade.php
+++ ProfilDesaPete/resources/views/admin/dashboard.blade.php
@@ -23,7 +23,7 @@
         <link href="{{url('css/bootstrap.min.css')}}" rel="stylesheet">
 
         <!-- Template Stylesheet -->
-        <link href="{{url('css/style.css')}}" rel="stylesheet">
+		<link href="{{url('css/style.css')}}" rel="stylesheet">
 
 		<!-- TinyMCE -->
 		<script src="https://cdn.tiny.cloud/1/jnkfs9zvfl13vly0k5556lcy3znx17cwejz9ng8k3hyk3uau/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
@@ -50,7 +50,7 @@
 	</head>
 <body>
    <!-- Topbar Start -->
-   <div class="container-fluid bg-primary px-5 d-none d-lg-block">
+   <div class="container-fluid bg-primary px-5 d-none d-lg-block topbar">
 		<div class="row gx-0 justify-content-end"> <!-- Tambahkan justify-content-end -->
    			<div class="col-lg-4 text-end"> <!-- Gunakan text-end agar teks sejajar ke kanan -->
                 <div class="d-inline-flex align-items-center" style="height: 45px;">
@@ -92,66 +92,47 @@
 	        </div>
 	    </div>
 	</div>
+
         <!-- Topbar End -->
-	<!-- <div class="wrapper">
-			<div class="sidebar">
-				<div class="scrollbar-inner sidebar-wrapper">
-					<ul class="nav">
-						<li class="nav-item active">
-							<a href="{{ url('admin') }}">
-								<p>Dashboard</p>
-								<span class="badge badge-count">5</span>
-							</a>
-						</li>
-						<li class="nav-item">
-								<i class="la la-table"></i>
-								<p>Components</p>
-								<span class="badge badge-count">14</span>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="forms.html">
-								<i class="la la-keyboard-o"></i>
-								<p>Forms</p>
-								<span class="badge badge-count">50</span>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="tables.html">
-								<i class="la la-th"></i>
-								<p>Tables</p>
-								<span class="badge badge-count">6</span>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="notifications.html">
-								<i class="la la-bell"></i>
-								<p>Notifications</p>
-								<span class="badge badge-success">3</span>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="typography.html">
-								<i class="la la-font"></i>
-								<p>Typography</p>
-								<span class="badge badge-danger">25</span>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="icons.html">
-								<i class="la la-fonticons"></i>
-								<p>Icons</p>
-							</a>
-						</li>
-						<li class="nav-item update-pro">
-							<button  data-toggle="modal" data-target="#modalUpdate">
-								<i class="la la-hand-pointer-o"></i>
-								<p>Update To Pro</p>
-							</button>
-						</li>
-					</ul>
-				</div>
-			</div> -->
+		<div class="sidebar">
+				<ul class="nav">
+					<li class="nav-item active">
+						<a href="{{ route('admin') }}">
+							<p>Dashboard</p>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="components.html">
+							<p>Components</p>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="forms.html">
+							<p>Forms</p>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="tables.html">
+							<p>Tables</p>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="notifications.html">
+							<p>Notifications</p>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="typography.html">
+							<p>Typography</p>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="icons.html">
+							<p>Icons</p>
+						</a>
+					</li>
+				</ul>
+		</div>
 
 			<div class="main-panel">
 					<div class="container-fluid">
